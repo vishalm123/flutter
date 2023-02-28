97d1f9e9592e5c4c578e3128f2ef4a24b183c216
vishalm123 <83279990+vishalm123@users.noreply.github.com>
2023-02-28 20:27:02 +0530

    Customizable Lists with ListView.Separated Flutter
    ListView.Separated is a widget in the Flutter framework that allows you to create a scrollable list of items with custom separators between them. It's similar to ListView.Builder, but instead of creating a single widget for each item in the list, it allows you to define two widgets - one for the item and one for the separator. This gives you more control over the appearance of the list, and allows you to create more complex layouts.
    
    With ListView.Separated, you can easily create a list of items with custom separators, such as horizontal dividers, vertical dividers, or any other widget that you want to use as a separator. You can also customize the appearance of each item in the list using the itemBuilder parameter, and customize the appearance of each separator using the separatorBuilder parameter.

---
+++ ListView_Separated.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Dart Program for ListView.builder'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    var arrNames = ["Ram","Ramesh","Raman"];
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('ListView.separated In dart/flutter'),
+        ),
+        body:
+          Center(
+            child:
+              ListView.separated(
+                itemBuilder: (context,index){
+                return Text(arrNames[index], style: TextStyle(fontSize: 21,fontWeight: FontWeight.w300),);
+              },
+              itemCount: arrNames.length, //to specify length of array
+              separatorBuilder: ((context, index) {
+                return Divider(height: 10,thickness: 4,);
+              }),
+          )
+        ) 
+    );
+  }
+}
