d89ce194d3c26a44f5b6c00d9a16a571f227da35
vishalm123 <83279990+vishalm123@users.noreply.github.com>
2023-03-04 15:33:43 +0530

    Flutter Custom Fonts: Unique Typography
    This topic focuses on using custom fonts in Dart's Flutter framework, which allows developers to add unique typography to their app. By importing and registering custom fonts, developers can create a distinctive visual identity for their app. Custom fonts can be used for various elements, such as text fields, headings, and buttons. Using custom fonts is a great way to enhance the design and brand recognition of an app.
    We can download fonts from "https://fonts.google.com/" for free.
    after downloading font we need to add dependences in pubspec.yaml and we are good to go.

---
+++ CustomFonts.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Learnings',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(title: 'Dart Program for Custom fonts'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Custom fonts in Dart/Flutter'),
+        ),
+        body:
+         Text("Hello Devs",style: TextStyle(fontFamily: 'FontMain',fontSize: 45),)
+    );
+  }
+}
