03c43312586e1646cf3e014187092c4ab9e70f37
vishalm123 <83279990+vishalm123@users.noreply.github.com>
2023-03-05 12:14:07 +0530

    Create Consistent UI with Themes in Flutter
    In Flutter, a `Theme` is a widget that allows you to define a consistent set of visual properties, such as colors, fonts, and text styles, that are applied to your app's UI. By using themes, you can ensure that your app has a consistent look and feel across different platforms and devices, and easily change the overall appearance of your app by updating the theme. With themes, you can also create custom themes that match your brand or design style, and easily apply them to your app's widgets.

---
+++ Theme.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Learnings',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        textTheme: TextTheme(//headline1 and subtitle1 are deprecated
+          //use alternative which are displayLarge and displayMedium
+          displayLarge:TextStyle(fontFamily: 'FontMain',fontSize: 40),
+          displayMedium: TextStyle(fontFamily: 'FontMain',fontSize: 20)
+          ),
+      ),
+      home: const MyHomePage(title: 'Dart Program for Theme Management'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Themes in Dart/Flutter'),
+        ),
+        body:
+        Column(children: [
+         Text("Hello Devs 1",style: Theme.of(context).textTheme.displayLarge),
+         Text("Hello Devs 2",style: Theme.of(context).textTheme.displayMedium),
+         Text("Hello Devs 3",style: Theme.of(context).textTheme.displayLarge),
+         Text("Hello Devs 4",style: Theme.of(context).textTheme.displayMedium),
+
+        ],
+        )
+    );
+  }
+}
