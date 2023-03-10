9c2b21ea6bde498644d1c1572404e0117a7cf441
vishalm123 <83279990+vishalm123@users.noreply.github.com>
2023-03-10 23:42:59 +0530

    Flutter GridView Extension: Flexible Layouts
    "GridView.extend" is not a recognized method or function in a specific programming language or framework, therefore it is not possible to provide a short description without additional context. Please provide more information about the programming language or framework you are using.

---
+++ Gridview_extend.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter code for Gridview.extend'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    body:GridView.extent(
+      maxCrossAxisExtent: 100,//specify space 
+      crossAxisSpacing: 11,//specify space between two container columnwise
+      mainAxisSpacing: 11,//specify space between two container row-wise
+      children: [
+        Container(color: Colors.red,),
+        Container(color: Colors.black,),
+        Container(color: Colors.green,),
+        Container(color: Colors.indigoAccent,),
+        Container(color: Colors.orange,),
+        Container(color: Colors.lightBlue,),
+        Container(color: Colors.blueAccent.shade100,),
+        Container(color: Colors.deepOrange,),
+      ],
+    ),
+    );
+  }
+}
