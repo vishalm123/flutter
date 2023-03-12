8ccc87cc35e946c886797cc9db96f2109f652362
vishalm123 <83279990+vishalm123@users.noreply.github.com>
2023-03-12 23:53:14 +0530

    Flutter Wrap: Flexible Layouts Made Easy
    Flutter Wrap widget provides an easy way to create flexible layouts that automatically wrap their children when the available space is filled. It simplifies the process of designing responsive interfaces that adapt to different screen sizes and orientations.

---
+++ Wrap.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'Widgets/DropDown.dart';
+import 'Widgets/input.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter code for Gridview.extend'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+  String dropdownValue = 'Option 1';
+    
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+    body:
+    Wrap(
+      direction: Axis.horizontal,
+      spacing: 11,//vertical spacing for Axis.horizontal
+      runSpacing: 11,//horizontal spacing for Axis.horizontal
+      children: [
+        Container(
+          width: 70,
+          height: 70,
+          color:  Colors.amber,
+        ),Container(
+          width: 70,
+          height: 70,
+          color:  Colors.red,
+        ),Container(
+          width: 70,
+          height: 70,
+          color:  Colors.green,
+        ),Container(
+          width: 70,
+          height: 70,
+          color:  Colors.black,
+        ),Container(
+          width: 70,
+          height: 70,
+          color:  Colors.brown,
+        ),Container(
+          width: 70,
+          height: 70,
+          color:  Colors.blueGrey,
+        ),
+      ],
+    )       
+    );
+  }
+}
