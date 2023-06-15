1c18be66d340d759f4f19df445abaf002263aaa0
vishalm123 <83279990+vishalm123@users.noreply.github.com>
2023-06-16 01:33:29 +0530

    Icons.dart
    In this code, we used List tile to display icons and their names

--- Icons.dart
+++ Icons.dart
@@ -1,52 +1,55 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  
-  // This widget is the root of your application.
+  // Define the list of icon data
+  final List<IconData> icons = [
+    Icons.ac_unit,
+    Icons.access_alarm,
+    Icons.access_time,
+    Icons.account_balance,
+    Icons.account_circle,
+    Icons.add_alert,
+    Icons.add_box,
+    Icons.add_circle,
+    Icons.airplanemode_active,
+    Icons.attach_file,
+  ];
+
+  // Define the list of icon names
+  final List<String> iconNames = [
+    'AC Unit',
+    'Access Alarm',
+    'Access Time',
+    'Account Balance',
+    'Account Circle',
+    'Add Alert',
+    'Add Box',
+    'Add Circle',
+    'Airplane Mode',
+    'Attach File',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello",
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Icon List'),
+        ),
+        body: ListView.builder(
+          itemCount: icons.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              leading: Icon(icons[index]),
+              title: Text(iconNames[index]),
+            );
+          },
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter code for Showing Icons'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-    body:
-    Center(
-      child:
-      Icon(
-        Icons.play_circle,
-        size: 50,
-        color:Colors.blue
-      )
-    )
-        
     );
   }
 }
